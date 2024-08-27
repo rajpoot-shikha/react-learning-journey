fb0bface85ce33935d24ef850828e0181c9e6ff4
Shikha Rajpoot <shikha.rajpoot1995@gmail.com>
2024-08-28 01:44:07 +0530

    Alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
@@ -9,10 +11,14 @@ const handleClick = (item: String) => {
 function App() {
   //ListGroup component can be reused for various kinds of lists.
   const colors = ["Red", "Yellow", "Blue", "Orange"];
+  const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <>
-      <Button color="danger" onClick={() => console.log("clicked")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>YAYY ALERT</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
         Button
       </Button>
       <ListGroup items={colors} title="Colors" onSelectItem={handleClick} />
---
+++ src/components/Alert.tsx
@@ -0,0 +1,26 @@
+interface Props {
+  children: string;
+  onClose: () => void;
+}
+
+function Alert({ children, onClose }: Props) {
+  return (
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
+      {children}
+      <button
+        type="button"
+        className="close"
+        data-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
+}
+
+export default Alert;
